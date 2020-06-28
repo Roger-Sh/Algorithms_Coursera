89fd2b2d79091d0f84c617c96f98bcf3bb519fb1
weipu_glb <weipu.shan@gmail.com>
2020-06-28 14:42:57 +0200

    shellsort

--- Algorithms_Coursera/src/Week2/ElementarySort/ShellSort.java
+++ Algorithms_Coursera/src/Week2/ElementarySort/ShellSort.java
@@ -1,28 +1,33 @@
 package Week2.ElementarySort;
 
-import edu.princeton.cs.algs4.StdOut;
-
 /*
-cost:
-    N^2/4 compares
-    N^2/4 exchanges
+Idea: Move entries more than one position at a time by h-sorting the array
 
-advantage for partially sorted situation
+cost:
+    worst case numbers of compares by shell sort with 3x+1 increments is N^(3/2)
  */
 
+import edu.princeton.cs.algs4.StdOut;
 
-public class InsertionSort {
-    // insertion sort,
-    // iterate from left to right,
-    //      exchange current element i with left element, till there is no bigger element on the left
+public class ShellSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
-        for (int i = 0; i < N; i++) {           // i move from left to right
-            for (int j = i; j > 0; j--) {   // j move from i to left
-                if (less(a[j], a[j - 1]))
-                    exch(a, j, j - 1);          // exchange the current element with its left bigger element
-                else break;                     // till there is only smaller elements on the left
+
+        // h-sorting by 3x+1 increment sequence, like: 13, 4, 1
+        int h = 1;
+        while (h < N / 3) {
+            h = 3 * h + 1;    // 1, 4, 13, 40, 121, 364... 3x+1 increment sequence
+        }
+
+        // for every h, partially sort by h-sorting
+        while (h >= 1) {
+            // h-sort the array
+            for (int i = h; i < N; i++) {       // insertion sort
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j - h);
+                }
             }
+            h = h / 3;        // move to next increment
         }
     }
 
@@ -54,11 +59,11 @@ public class InsertionSort {
         Date day3 = new Date(6, 26, 2020);
         Date[] days = {day1, day2, day3};
 
-        InsertionSort.sort(days);
+        ShellSort.sort(days);
 
         for (int i = 0; i < days.length; i++) {
             StdOut.println(days[i].year + "_" + days[i].month + "_" + days[i].day);
         }
-        StdOut.println("This array is sorted? " + InsertionSort.isSorted(days));
+        StdOut.println("This array is sorted? " + ShellSort.isSorted(days));
     }
 }
